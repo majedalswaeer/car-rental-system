eada4d750d576c7b7fe05d06960d6efa0b49b053
mswaeer <mswaeer@ainnova.com>
2025-06-11 14:43:44 +0300

    Add api-gateway

---
+++ api-gateway/src/test/java/com/majed/carrental/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majed.carrental.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
